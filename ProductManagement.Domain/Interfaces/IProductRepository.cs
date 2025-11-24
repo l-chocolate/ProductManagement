bfb3f8aa18bb9ce05e7bb84046b1f119d11fe9b2
Lucas de Oliveira do Nascimento <lu-nascimento@live.com>
2025-11-24 00:30:47 -0300

    chore: Cria estrutura inicial do projeto

---
+++ ProductManagement.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using ProductManagement.Domain.Entities;
+
+namespace ProductManagement.Domain.Interfaces
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+    }
+}
---
+++ ProductManagement.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using ProductManagement.Domain.Entities;
+
+namespace ProductManagement.Domain.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : Entity
+    {
+        Task<TEntity> Add(TEntity entity);
+        Task<TEntity> Update(TEntity entity);
+        Task Delete(TEntity entity);
+        Task<TEntity?> GetById(int id);
+    }
+}
