136b340e868d5f32ae6f7a66a21d1e366fd20fe4
Lucas de Oliveira do Nascimento <lu-nascimento@live.com>
2025-11-25 05:10:55 -0300

    feat: Faz ajustes gráficos

--- product-management-frontend/src/app/app.ts
+++ product-management-frontend/src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
